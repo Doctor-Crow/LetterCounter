ecc1649e4702afcced6fe757b41685b5512f48b8
Dmitriy Sokolov <78034102+Doctor-Crow@users.noreply.github.com>
2021-04-11 20:50:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-chars = {}
-text = input()
+while True:
+ chars = {}
+ text = input()
+ for char in text:
+     if char.isalpha():
+         if char.lower() in chars:
+             chars[char.lower()] += 1
+         else:
+             chars[char.lower()] = 1
 
-for char in text:
-    if char.isalpha():
-        if char.lower() in chars:
-            chars[char.lower()] += 1
-        else:
-            chars[char.lower()] = 1
-
-print(chars)
+ print(chars)
