57d5d781576e6d4c17dca93d304ebc0d95a5f0c4
Dmitriy Sokolov <s0k0l7777@mail.ru>
2021-04-11 20:27:16 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+chars = {}
+text = input()
+
+for char in text:
+    if char.isalpha():
+        if char.lower() in chars:
+            chars[char.lower()] += 1
+        else:
+            chars[char.lower()] = 1
+
+print(chars)
